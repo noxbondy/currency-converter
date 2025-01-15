293f5c79aa7242790ecfd2d224f6b55f535d5973
Md gulam Noxboundy <gulamnoxbondy@gmail.com>
2025-01-15 14:42:25 +0100

    Add files via upload

--- convater.java
+++ convater.java
@@ -1,8 +1,50 @@
-
-public  class  Main{
+import java.util.Scanner;
+public class convater {
     public static void main(String[] args) {
-        String[] cars = {"volo", "BMW", "Mazda"};
-        
-        System.out.println (cars.length);
+        System.out.println("1 Krona");
+        System.out.println("2 Dollar");
+        System.out.println("3 Euro");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose the currency");
+        int choice = sc.nextInt();
+
+        double amount = sc.nextDouble();
+
+        double euro = sc.nextDouble();
+
+        switch (choice) {
+            case 1:
+                krona(amount);
+                break;
+                case 2:
+                    krona(amount);
+                    break;
+                    case 3:
+                        krona(amount);
+                        break;
+        }
+
+
+    }
+    public static void krona(double amount) {
+        System.out.println("1 krona " + 0.090+ "dollar");
+        System.out.println();
+
+        System.out.println(amount+" krona = " + (amount*0.090) + " Dollar");
+        System.out.println();
+
+        System.out.println(amount+" krona = " + (amount*0.086) + " Euro");
+        System.out.println();
+
+
+    }
+    public static void dollar(double amount) {
+        System.out.println("1 Dollar");
+        System.out.println();
+        System.out.println(amount+" dollar = " + (amount*0.086) + " Euro");
+        System.out.println();
+
+
     }
 }
