327cd8110f285d7b2b3bde84a32ad7e5d6766685
Md gulam Noxboundy <gulamnoxbondy@gmail.com>
2025-01-15 09:26:48 +0100

    Add files via upload

---
+++ convater.java
@@ -0,0 +1,8 @@
+
+public  class  Main{
+    public static void main(String[] args) {
+        String[] cars = {"volo", "BMW", "Mazda"};
+        
+        System.out.println (cars.length);
+    }
+}
